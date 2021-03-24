c29a3f79934fd6425910706682ca5e7cf792a0c2
bartaodi <barta.kod@gmail.com>
2021-03-24 17:45:33 +0100

    sum of odd numbers done

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -1,10 +1,23 @@
 package com.codecool.sum_of_odd_numbers;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        List<Integer> oddNumbers = new ArrayList<>();
+        for (Integer i : numbers) {
+            if (i % 2 != 0) {
+                oddNumbers.add(i);
+            }
+        }
+
+        Integer sumOfOddNum = 0;
+        for (Integer i : oddNumbers) {
+            sumOfOddNum += i;
+        }
+        return sumOfOddNum;
     }
 }
